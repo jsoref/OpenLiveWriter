38065b7bb45047732ba210741935ccb1348bfbc0
Josh Soref <apache@soref.com>
2015-12-11 20:33:12 +0000

    spelling: register

--- src/managed/OpenLiveWriter.PostEditor/ContentEditor/ContentEditor.cs
+++ src/managed/OpenLiveWriter.PostEditor/ContentEditor/ContentEditor.cs
@@ -1691,7 +1691,7 @@ namespace OpenLiveWriter.PostEditor
             IHtmlEditorComponentContext componentContext = null;
             if (_currentEditor != null)
             {
-                //unregisiter from editing events
+                //unregister from editing events
                 _currentEditor.TitleChanged -= new EventHandler(_currentEditor_TitleChanged);
                 _currentEditor.EditableRegionFocusChanged -= new EventHandler(_currentEditor_EditableRegionFocusChanged);
 
