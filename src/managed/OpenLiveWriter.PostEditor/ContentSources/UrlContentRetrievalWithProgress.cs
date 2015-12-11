a5dd1a15c3fc53c7aa3bca60deca81708de8f5d2
Josh Soref <apache@soref.com>
2015-12-11 17:41:09 +0000

    spelling: retrieve

--- src/managed/OpenLiveWriter.PostEditor/ContentSources/UrlContentRetrievalWithProgress.cs
+++ src/managed/OpenLiveWriter.PostEditor/ContentSources/UrlContentRetrievalWithProgress.cs
@@ -186,13 +186,13 @@ namespace OpenLiveWriter.PostEditor.ContentSources
             get { return _contentSource; }
         }
 
-        protected abstract void RetreiveContent(ref string title);
+        protected abstract void RetrieveContent(ref string title);
 
         protected override void DoWork()
         {
             try
             {
-                RetreiveContent(ref _title);
+                RetrieveContent(ref _title);
             }
             catch (OperationCancelledException)
             {
@@ -223,7 +223,7 @@ namespace OpenLiveWriter.PostEditor.ContentSources
             get { return _newContent; }
         }
 
-        protected override void RetreiveContent(ref string title)
+        protected override void RetrieveContent(ref string title)
         {
             (ContentSource as ContentSource).CreateContentFromUrl(Url, ref title, ref _newContent);
         }
@@ -240,7 +240,7 @@ namespace OpenLiveWriter.PostEditor.ContentSources
             _newContent = newContent;
         }
 
-        protected override void RetreiveContent(ref string title)
+        protected override void RetrieveContent(ref string title)
         {
             (ContentSource as SmartContentSource).CreateContentFromUrl(Url, ref title, _newContent);
         }
